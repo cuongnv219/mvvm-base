6711c8b5be0b307c92d81cb4357f60f3faf85076
cuongnv <cuongnv219@gmail.com>
2018-07-24 11:02:04 +0700

    Update rxjava

--- mvvm-base/src/main/java/com/base/utils/ImageLoaderUtils.java
+++ mvvm-base/src/main/java/com/base/utils/ImageLoaderUtils.java
@@ -1,13 +1,5 @@
 package com.base.utils;
 
-import android.content.Context;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
-
 /**
  * cuongnv
  * 5/30/18
@@ -18,27 +10,27 @@ public final class ImageLoaderUtils {
 
     }
 
-    public static void show(ImageView iv, String url) {
-        Context c = iv.getContext();
-
-        RequestOptions options = new RequestOptions()
-                .centerCrop()
-//                .error(R.drawable.image_not_found)
-                .transform(new RoundedCorners(5))
-                .priority(Priority.HIGH);
-        Glide.with(c).load(url).apply(options)
-                .into(iv);
-    }
-
-    public static void show(ImageView iv, int url) {
-        Context c = iv.getContext();
-
-        RequestOptions options = new RequestOptions()
-                .centerCrop()
-//                .error(R.drawable.bg_world_cup)
-                .transform(new RoundedCorners(5))
-                .priority(Priority.HIGH);
-        Glide.with(c).load(url).apply(options)
-                .into(iv);
-    }
+//    public static void show(ImageView iv, String url) {
+//        Context c = iv.getContext();
+//
+//        RequestOptions options = new RequestOptions()
+//                .centerCrop()
+////                .error(R.drawable.image_not_found)
+//                .transform(new RoundedCorners(5))
+//                .priority(Priority.HIGH);
+//        Glide.with(c).load(url).apply(options)
+//                .into(iv);
+//    }
+//
+//    public static void show(ImageView iv, int url) {
+//        Context c = iv.getContext();
+//
+//        RequestOptions options = new RequestOptions()
+//                .centerCrop()
+////                .error(R.drawable.bg_world_cup)
+//                .transform(new RoundedCorners(5))
+//                .priority(Priority.HIGH);
+//        Glide.with(c).load(url).apply(options)
+//                .into(iv);
+//    }
 }
